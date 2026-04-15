ab2102433dd2b01e63656912db2d4e52dcce55bb
Antragon <antonin1.beltran@gmail.com>
2026-04-15 13:05:41 +0200

    début fonctions menus

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ fenetre.title("Mastermind")
 menu = Menu(fenetre)
 fenetre.config(menu = menu)
 
+#fonctions pour les menus
+def charge_partie():
+    """Permet de charger le fichier sauvegarde"""
+    pass
+
+def sauve_partie():
+    """Permet de sauvegarder la partie en cours dans un fichier"""
+    pass
+
+def supr_partie():
+    """Permet de supprimer une sauvegarde"""
+    pass
+
 # paramètres
 menu_parametre= Menu(menu, tearoff = 0)
 ai_var = StringVar(value="knuth")
@@ -15,14 +28,14 @@ menu_ia = Menu(menu_parametre, tearoff=0)
 menu_ia.add_radiobutton(label="Knuth's Algorithm", variable=ai_var, value="knuth")
 menu_ia.add_radiobutton(label="Expected-Size Algorithm", variable=ai_var, value="exp_size")
 menu.add_cascade(label = "Paramètres", menu = menu_parametre)
-menu_parametre.add_command(label = "Ouvrir", command = None)
+menu_parametre.add_command(label = "Ouvrir", command = ouvrir)
 menu_parametre.add_cascade(label = "IA", menu = menu_ia)
 
 #fichier
 menu_fichier = Menu(menu, tearoff = 0)
 menu.add_cascade(label = "Fichier", menu = menu_fichier)
-menu_fichier.add_command(label = "Sauvegarder", command = None)
-menu_fichier.add_command(label = "Charger", command = None)
-menu_fichier.add_command(label = "Supprimer", command = None)
+menu_fichier.add_command(label = "Sauvegarder", command = sauve_partie)
+menu_fichier.add_command(label = "Charger", command = charge_partie)
+menu_fichier.add_command(label = "Supprimer", command = supr_partie)
 
 fenetre.mainloop()
