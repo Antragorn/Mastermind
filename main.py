cfddcb650e6616685e98683555955e99b330122d
Antragon <antonin1.beltran@gmail.com>
2026-04-15 12:58:13 +0200

    ajout du choix d'IA

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ from tkinter import *
 fenetre = Tk()
 fenetre.title("Mastermind")
 
-#création des menues
+#création des menus
 menu = Menu(fenetre)
 fenetre.config(menu = menu)
 
 # paramètres
 menu_parametre= Menu(menu, tearoff = 0)
+ai_var = StringVar(value="knuth")
+menu_ia = Menu(menu_parametre, tearoff=0)
+menu_ia.add_radiobutton(label="Knuth's Algorithm", variable=ai_var, value="knuth")
+menu_ia.add_radiobutton(label="Expected-Size Algorithm", variable=ai_var, value="exp_size")
 menu.add_cascade(label = "Paramètres", menu = menu_parametre)
-menu_parametre.add_command(label = "ouvrir", command = None)
+menu_parametre.add_command(label = "Ouvrir", command = None)
+menu_parametre.add_cascade(label = "IA", menu = menu_ia)
 
 #fichier
 menu_fichier = Menu(menu, tearoff = 0)
-menu.add_cascade(label = "fichier", menu = menu_fichier)
+menu.add_cascade(label = "Fichier", menu = menu_fichier)
 menu_fichier.add_command(label = "Sauvegarder", command = None)
 menu_fichier.add_command(label = "Charger", command = None)
 menu_fichier.add_command(label = "Supprimer", command = None)
